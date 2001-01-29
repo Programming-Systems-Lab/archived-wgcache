a52aec07633614b22bb36a6821abd966b43be6d0
ajs248 <ajs248>
2001-01-29 00:32:02 +0000

    adding the rule part

---
+++ impl/RMI_PCMImpl.java
@@ -0,0 +1,25 @@
+package psl.wgcache.impl;
+
+/** Copyright (c) 2000: The Trustees of Columbia University and the City of New York.
+ * All Rights Reserved.
+ * 
+ * 
+ * @author  Alpa
+ *  
+ */
+ 
+import psl.wgcache.exception.*;
+import psl.wgcache.roles.*;
+import java.rmi.*;
+import java.rmi.server.*;
+public class RMI_PCMImpl extends UnicastRemoteObject implements RMI_PCM {
+  PersonalCacheModuleImpl parent;   RMI_PCMImpl(PersonalCacheModuleImpl pcmi) throws RemoteException {      parent = pcmi;
+    try {
+      Naming.rebind(parent.getName(), this);
+    } catch (Exception e) {
+      System.out.println("ERROR: RMI_PCMimpl could not register");
+      e.printStackTrace();
+    }   }  public Cacheable query(Object queryData) throws WGCException, RemoteException {    return (parent.query(queryData));
+  }      public void WGCPut(Cacheable data)throws RemoteException {
+    parent.genericPut(data);  }
+}   
