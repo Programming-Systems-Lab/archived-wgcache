e8466d061ea57473215c3e9c060bb3ee29d8366d
gskc <gskc>
2001-02-07 01:35:01 +0000

    Changes made at Monterey.

--- impl/Cacheable.java
+++ impl/Cacheable.java
@@ -1,6 +1,6 @@
 package psl.wgcache.impl;
 
-import psl.*;
+//gskc: import psl.*;
 import java.util.*;
 
 public class Cacheable implements java.io.Serializable { 
@@ -9,11 +9,15 @@ public class Cacheable implements java.io.Serializable {
     this.data = null;
     this.size = 0;
   }
-  public Cacheable(Object k,Object d,long s){     this.key = k;
+  public Cacheable(Object k,Object d,long s){ 
+    this.key = k;
     this.data = d;
     this.size = s;
   }  
-  public String toString() {    return "Cacheable: (" + key + ", " + data + ", " + size + ") ";
-  }  public Object key;  public Object data;
+  public String toString() {
+    return "Cacheable: (" + key + ", " + data + ", " + size + ") ";
+  }
+  public Object key;
+  public Object data;
   public long size;
 }
--- support/CriteriaInfoImpl.java
+++ support/CriteriaInfoImpl.java
@@ -57,36 +57,42 @@ public class CriteriaInfoImpl  implements CriteriaInfo,java.io.Serializable {
 
   public Module[] getMembers()  {
     Module[] retval = null;
+    /*
     if(receiver instanceof WorkgroupImpl) {
       retval = ((WorkgroupImpl)receiver).members();
     } else {
       retval = new Module[0];
     }
+    */
     return retval;
   }
-	public Module getToBePushed() {		return toBePushedModule;	}
+
+	public Module getToBePushed() {
+		return toBePushedModule;
+	}
 
 
   public Module[] getWorkgroups()  {
     Module[] retval = null;
+    /*
     if(receiver instanceof PersonalCacheModuleImpl) {
       retval = ((PersonalCacheModuleImpl)receiver).workgroups();
     } else {
       retval = new Module[0];
     }
+    */
     return retval;
   }
 
   public void push(Cacheable x, Module m)  {
     // Don't push it back to where it came from
 		log("In the push while applying criteria with module name :" + m.getName() + "and the recieved from module :" + receiver);
-		if(!m.getName().equals(receiver))
-			m.pushTo(trace, x);
+		// if(!m.getName().equals(receiver)) m.pushTo(trace, x);
   }
 
   public Cacheable pull(String name, Module m)    throws WGCException  {
     Cacheable result = null;
-    result = m.pullFrom(trace, name);
+    // result = m.pullFrom(trace, name);
     return result;
   }
 	private void log(String mesg){
