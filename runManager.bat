f1bc30d984f1f5e1e69a23f32caf97f59ec5b39f
gskc <gskc>
2001-06-04 18:21:19 +0000

    each PCM will try to start Rmireg ... is not harmful at all

--- runManager.bat
+++ runManager.bat
@@ -1,2 +1,7 @@
+@echo off
+cd\pslcvs
+echo Starting rmiregistry
+start /min rmiregistry
+
 echo Executing Workgroup Cache - the Workgroup Manager
 java -DSpecfile=c:\pslcvs\psl\xues\WGCRules.xml -classpath c:\pslcvs\jars\wgc.jar;c:\pslcvs\jars\siena-1.1.2.jar;c:\pslcvs\jars\xerces.jar psl.wgcache.WGC_Manager_Visual
--- runPCMGail.bat
+++ runPCMGail.bat
@@ -1,2 +1,3 @@
 echo Executing Workgroup Cache - the Personal Cache Module
+start rmiregistry
 java -DUser=Gail -DManager=rmi://varick/manager -Djava.security.policy=c:\pslcvs\psl\wgcache\pcm.policy -classpath c:\pslcvs\jars\wgc.jar;c:\pslcvs\jars\hsql.jar psl.wgcache.PCM_Visual
