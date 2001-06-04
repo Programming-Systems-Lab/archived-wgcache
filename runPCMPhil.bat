cfb7efa433ca8acadced69a875fb50c453b27afc
gskc <gskc>
2001-06-04 15:33:50 +0000

    *** empty log message ***

---
+++ runPCMPhil.bat
@@ -0,0 +1,2 @@
+echo Executing Workgroup Cache - the Personal Cache Module
+java -DUser=Phil -DManager=rmi://varick/manager -Djava.security.policy=c:\pslcvs\psl\wgcache\pcm.policy -classpath c:\pslcvs\jars\wgc.jar;c:\pslcvs\jars\hsql.jar psl.wgcache.PCM_Visual
